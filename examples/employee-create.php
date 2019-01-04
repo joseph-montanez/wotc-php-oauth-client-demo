e7dc3b6b2bc2487c55a68412d862a1647540b9d3
Joseph Montanez <sutabi@gmail.com>
2019-01-03 17:08:12 -0800

    added address

--- examples/employee-create.php
+++ examples/employee-create.php
@@ -10,7 +10,7 @@ $api = get_api();
 $result = $api->post(
     'employees',
     [
-        'first_name'  => "John",
+        'first_name' => "John",
         'last_name'  => "Doe",
         "email"      => "johndoe@gmail.com",
         "phone"      => "619-555-5555",
@@ -19,6 +19,29 @@ $result = $api->post(
         "dob"        => "1980-01-01",
     ]
 );
-
 print_r($result);
 $employee_id = $result['id'];
+
+//---------------------------------
+//-- Create Employee - With Address
+//---------------------------------
+$result2 = $api->post(
+    'employees',
+    [
+        'first_name'      => "John",
+        'last_name'       => "Apple",
+        "email"           => "johnapple@gmail.com",
+        "phone"           => "619-555-1234",
+        "ssn"             => "2234",
+        "started_on"      => "11/10/2018",
+        "dob"             => "1981-01-01",
+        "address_street"  => "123 Street",
+        "address_street2" => "Apt 16",
+        "address_city"    => "Vista",
+        "address_state"   => "CA",
+        "address_county"  => "San Diego",
+        "address_zipcode" => "92083",
+    ]
+);
+print_r($result2);
+$employee_id2 = $result2['id'];
---
+++ examples/register.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/config.php';
+
+$api = get_api();
+
+//---------------------------------
+//-- Create Employee
+//---------------------------------
+$ein    = rand(10, 99) . '-' . rand(1000000, 9999999);
+$result = $api->post(
+    'register',
+    [
+        'first_name'   => "John",
+        'last_name'    => "Doe",
+        "email"        => "johndoe+$ein@gmail.com",
+        "phone"        => "619-555-5555",
+        "ein"          => $ein,
+        "website"      => "https://google.com",
+        "company_name" => "Test $ein",
+        'address'      => "123 Street",
+        'city'         => "New York",
+        'state'        => "New York",
+        'zipcode'      => "92000",
+        'for_profit'   => true,
+    ]
+);
+
+print_r(json_encode($result['user']));
